fefed2c6524f6fa71974d564894caad505bd79af
Hein Htet Zaw <maintain1346@gmail.com>
2025-10-09 23:20:40 +0630

    added option to read from pyproject file when version can't be read for version test

--- tests/test_version_output.py
+++ tests/test_version_output.py
@@ -2,14 +2,22 @@ from typer.testing import CliRunner
 from importlib.metadata import version, PackageNotFoundError
 
 from sshu.cli import app
+from pathlib import Path
 
 runner = CliRunner()
 
 def test_cli_version():
     result = runner.invoke(app,["version"])
+    project_file: Path = Path.cwd() / "pyproject.toml"
+    project_file_contents = project_file.read_text().splitlines()
+    version_from_file = "0.0.0"
+    for line in project_file_contents:
+        if "version" in line:
+            version_from_file = line.split(" = ")[1].replace('"','')
+            break
     try:
         __version__ = version("sshu")
     except PackageNotFoundError:
-        __version__ = "0.0.0"
+        __version__ = version_from_file
     assert result.exit_code == 0
     assert result.stdout.strip() == __version__
