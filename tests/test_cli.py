0c60abb6aa3fe9137d881d36e22dc8bc511a8deb
Hein Htet Zaw <maintain1346@gmail.com>
2026-05-09 14:38:09 +0630

    Bump version to 1.0.0-beta, refactor CLI arguments, and finalize test suite

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,15 @@ runner = CliRunner()
 
 def test_cli_add_command(temp: tuple, monkeypatch):
     ssh_cfg: Path = temp[0]
+    temp_dir: Path = temp[1]
+    sshu_cfg_file: Path = temp[2]
+    
+    (temp_dir / "id_ed25519").touch()
+    
+    from sshu import cli
+    monkeypatch.setattr(cli, "ssh_dir", temp_dir)
+    monkeypatch.setattr(cli, "sshu_cfg_dir", temp_dir)
+    monkeypatch.setattr(cli, "sshu_cfg_file", sshu_cfg_file)
     
     from sshu.conn import manager
     monkeypatch.setattr(manager, "ssh_cfg", ssh_cfg)
@@ -21,13 +30,33 @@ def test_cli_add_command(temp: tuple, monkeypatch):
     assert "User cliapp" in cfg_content
     assert "HostName 192.168.99.99" in cfg_content
 
-def test_cli_add_mutually_exclusive_auth(temp: tuple):
+def test_cli_add_mutually_exclusive_auth(temp: tuple, monkeypatch):
+    ssh_cfg: Path = temp[0]
+    temp_dir: Path = temp[1]
+    sshu_cfg_file: Path = temp[2]
+    (temp_dir / "id_ed25519").touch()
+    
+    from sshu import cli
+    monkeypatch.setattr(cli, "ssh_dir", temp_dir)
+    monkeypatch.setattr(cli, "sshu_cfg_dir", temp_dir)
+    monkeypatch.setattr(cli, "sshu_cfg_file", sshu_cfg_file)
+    
     # Testing that --passwd and --keypair fail when used together
     result = runner.invoke(app, ["add", "fail_server", "-u", "admin", "-a", "1.1.1.1", "--passwd", "--keypair", "dummy.pem"])
     assert result.exit_code == 1
     assert "You can't use both --passwd and --keypair" in result.output
 
-def test_cli_add_missing_auth(temp: tuple):
+def test_cli_add_missing_auth(temp: tuple, monkeypatch):
+    ssh_cfg: Path = temp[0]
+    temp_dir: Path = temp[1]
+    sshu_cfg_file: Path = temp[2]
+    (temp_dir / "id_ed25519").touch()
+    
+    from sshu import cli
+    monkeypatch.setattr(cli, "ssh_dir", temp_dir)
+    monkeypatch.setattr(cli, "sshu_cfg_dir", temp_dir)
+    monkeypatch.setattr(cli, "sshu_cfg_file", sshu_cfg_file)
+    
     # Testing that missing both auth options fails
     result = runner.invoke(app, ["add", "fail_server", "-u", "admin", "-a", "1.1.1.1"])
     assert result.exit_code == 1
