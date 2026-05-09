ea95fa29ab81a37c91bacbd1626046a38e12417e
Hein Htet Zaw <maintain1346@gmail.com>
2026-05-09 14:33:04 +0630

    bump version to 1.0.0

--- src/sshu/cli.py
+++ src/sshu/cli.py
@@ -44,7 +44,7 @@ def show_version():
     try:
         __version__ = importlib.metadata.version("sshu")
     except importlib.metadata.PackageNotFoundError:
-        __version__ = "0.1.4-beta"
+        __version__ = "1.0.0-beta"
     print(__version__)
 
 @app.callback(invoke_without_command=True)
