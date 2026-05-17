9ba6a773770f4b943b05e43f72b7dcfd254cab21
Hein Htet Zaw <maintain1346@gmail.com>
2026-05-17 21:42:03 +0630

    chore: bump version in cli.py

--- src/sshu/cli.py
+++ src/sshu/cli.py
@@ -44,7 +44,7 @@ def show_version():
     try:
         __version__ = importlib.metadata.version("sshu")
     except importlib.metadata.PackageNotFoundError:
-        __version__ = "1.0.0-beta"
+        __version__ = "1.0.0-beta1"
     print(__version__)
 
 @app.callback(invoke_without_command=True)
